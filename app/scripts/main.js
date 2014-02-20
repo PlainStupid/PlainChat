8f4f52914189e1c4f9eb23cccaf160e7d47a3194
kristinnv12 <kristinnv12@ru.is>
2014-02-20 19:16:55 +0000

    thurfti ad faera node_moduels til ad refrenca

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,39 @@ app.config(function($routeProvider) {
 	})
 	.otherwise({ redirectTo: "/" });
 });
-app.controller("LoginCtrl", function($scope) {
-	$scope.message = "login";
+/*
+app.controller("LoginCtrl", ["$scope", "SocketService", function($scope, SocketService) {
+	
+	$scope.message = "test";
+	
+	var socket = io.connect('http://localhost:8080');
 
-});
+	$scope.connect = function() {
+		socket.emit("adduser", $scope.username, function(available){
+			if(available)
+			{
+				window.alert("available");
+			}
+		}
+
+	}
+
+}]);*/
+
+
+app.controller("LoginCtrl", ["$scope", function($scope) {
+
+	$scope.mymessage = "wat";
+	
+	var socket = io.connect('http://localhost:8080');
+	window.alert("available");
+/*
+	$scope.connect = function() {
+		socket.emit("adduser", $scope.username, function(available){
+			if(available)
+			{
+				window.alert("available");
+			}
+		}*/
+
+}]);
