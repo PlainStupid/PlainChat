bae0526c480b065e60c5e118f8bb80e34cfe2a89
ragnarp12 <ragnarp12@ru.is>
2014-02-25 20:44:57 +0000

    Kick added

--- app/scripts/controllers/room.js
+++ app/scripts/controllers/room.js
@@ -2,46 +2,14 @@ app.controller('RoomCtrl', ['$scope', '$location', '$routeParams', 'SocketSrv', 
     function($scope, $location, $routeParams, SocketSrv, PLAIN_URL) {
 
         var socket = SocketSrv.getSocket();
-        //$scope.roomName = $routeParams.roomName;
-        /*$scope.roomName = 'Næsroom';
 
-        $scope.messageHistory = [{
-            nick: 'Ragnar',
-            timestamp: '1288323623006',
-            message: 'Flott skilaboð'
-        }, {
-            nick: 'Kristinn',
-            timestamp: '1288323628888',
-            message: 'Flott skilaboð'
-        }, {
-            nick: 'Ragnar',
-            timestamp: '1288343323006',
-            message: 'Flott skilaboð'
-        }, {
-            nick: 'Jesús',
-            timestamp: '1288523623006',
-            message: 'Flott skilaboð'
-        }];
-
-        $scope.uss = [{
-            username: 'ragnar'
-        }, {
-            username: 'kiddi'
-        }];
-
-        $scope.users = $scope.uss;
-
-        $scope.roomMessages = $scope.messageHistory;
-
-        */
-
-        //$scope.users = '';
-        //
         $scope.glued = true;
 
         $scope.roomName = $routeParams.roomId;
 
         $scope.ops = [];
+
+        var kick = /^\/kick\s.+$/;
 
         if (socket) {
 
@@ -63,18 +31,34 @@ app.controller('RoomCtrl', ['$scope', '$location', '$routeParams', 'SocketSrv', 
                     $scope.$apply();
                 }
             });
+
         }
 
         $scope.sendMessage = function() {
             if (socket) {
+                var parameterIndex = $scope.usermessage.indexOf(' ') + 1;
+                thevictim = $scope.usermessage.substring(parameterIndex, $scope.usermessage.length);
 
-                socket.emit('sendmsg', {
-                    roomName: $scope.roomName,
-                    msg: $scope.usermessage
-                });
+                if (kick.test($scope.usermessage)) {
+                    socket.emit('kick', {
+                        room: $scope.roomName,
+                        user: thevictim
+                    }, function(sucess, err) {
+                        if (success) {
+                            $scope.successText = 'You kicked ' + thevictim;
+                            $scope.showSuccess = true;
+                        }
+                    });
 
-                $scope.successText = $scope.usermessage;
-                $scope.showSuccess = true;
+                } else {
+                    socket.emit('sendmsg', {
+                        roomName: $scope.roomName,
+                        msg: $scope.usermessage
+                    });
+                }
+
+                //$scope.successText = $scope.usermessage;
+                //$scope.showSuccess = true;
                 $scope.usermessage = '';
             }
 
@@ -83,28 +67,5 @@ app.controller('RoomCtrl', ['$scope', '$location', '$routeParams', 'SocketSrv', 
         $scope.switchBool = function(value) {
             $scope[value] = !$scope[value];
         };
-
-        /*
-
-    if (socket) {
-
-        socket.emit('joinroom', {
-            room: $scope.roomName
-        }, function(success, err));
-
-        $scope.roomMessages = $scope.messageHistory;
-
-        socket.on('updateusers', function(room, users) {
-                if (room === $scope.roomName) {
-                    $scope.users = users;
-                }
-            });
-    });*/
-        /*
-
-        nick : socket.username,
-        timestamp :  new Date(),
-        message : data.msg.substring(0, 200)
-        }*/
     }
 ]);
